74b4d6a822986d29079427f4e9ed3db33f2f4b73
Mirko-1012 <di.natale.mirko.9@gmail.com>
2026-06-05 10:03:39 +0200

    aggiunto <> in remote

--- 02_Remote/SmartHome.java
+++ 02_Remote/SmartHome.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class SmartHome {
-    private ArrayList<RemoteControl> remoteControlList = new ArrayList();
+    private ArrayList<RemoteControl> remoteControlList = new ArrayList<>();
 
     public void add(RemoteControl rc) {
         this.remoteControlList.add(rc);
